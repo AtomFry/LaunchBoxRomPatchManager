d18ef275925800e088ca4de96f31b67eb9d02ec1
SuplexLuthor <adamfry78@gmai.com>
2021-12-22 15:21:36 -0600

    fixed file copy to overwrite

--- LaunchBoxRomPatchManager/Helpers/DirectoryInfoHelper.cs
+++ LaunchBoxRomPatchManager/Helpers/DirectoryInfoHelper.cs
@@ -361,7 +361,7 @@ namespace LaunchBoxRomPatchManager.Helpers
             foreach (FileInfo file in files)
             {
                 string tempPath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(tempPath, false);
+                file.CopyTo(tempPath, true);
             }
 
             // If copying subdirectories, copy them and their contents to new location.
